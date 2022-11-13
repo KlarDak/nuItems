a35efbe1d59e3e0f4985e6db7f579172aef2d2ac
KlarDak <mk_daking@mail.ru>
2022-11-14 01:23:14 +0300

    0.1 Dev
    - nuInputs: класс для создания полей ввода готов.

--- build/ts/nuInputs.ts
+++ build/ts/nuInputs.ts
@@ -2,13 +2,69 @@ interface InputFieldConfig {
     type: string,
     placeholder: string,
     id?: string,
-    className?: string|Array<string>,
+    className?: string,
     name?: string,
-    isRequired: bool
+    isRequired: boolean
 }
 
 class nuInputs {
-    public createInputField(root: HTMLElement, config: InputFieldConfig) {
+    static nuElem: HTMLElement;
 
+    public static addInputField(root: HTMLElement, config: InputFieldConfig) {
+        this.__addInputFieldBox(config.className);
+        this.__addInputLabel(config.placeholder, config.className);
+        this.__addInputField(config.type, config.className, config.name, config.id, config.placeholder);
+
+        if (config.isRequired === true)
+            this.__addInputRequiredLabel();
+
+        root.appendChild(this.nuElem);
+    }
+
+    private static __addInputFieldBox(className?: string) : void {
+        this.nuElem = this.__createElem("div", "inputFieldBlock", className);
+    }
+
+    private static __addInputLabel(placeholder: string, classLabel?: string) {
+         let inputFieldLabel = this.__createElem("label", "inputFieldLabel", classLabel);
+         inputFieldLabel.innerText = placeholder;
+
+         this.nuElem.appendChild(inputFieldLabel);
+    }
+
+    private static __addInputField(type: string, className?: string, name?: string, id?: string, placeholder?: string) {
+        let inputField = document.createElement("input");
+        inputField.type = type ?? "text";
+        inputField.className = "inputField";
+
+        if (className)
+            inputField.classList.add(className);
+
+        inputField.id = this.__checkArg(id);
+        inputField.name = this.__checkArg(name);
+        inputField.placeholder = this.__checkArg(placeholder);
+
+        this.nuElem.appendChild(inputField);
+    }
+
+    private static __addInputRequiredLabel() {
+        let inputFieldRequired = this.__createElem("label", "inputFieldRequired");
+        inputFieldRequired.innerHTML = "Обязательное поле";
+
+        this.nuElem.appendChild(inputFieldRequired);
+    }
+
+    private static __createElem(typeElem: string, classNameElem: string, anotherClassElem?: string) {
+        let newNuElem = document.createElement(typeElem);
+        newNuElem.className = classNameElem;
+
+        if (anotherClassElem)
+            newNuElem.classList.add(anotherClassElem);
+
+        return newNuElem;
+    }
+
+    private static __checkArg(arg: string) : string|null {
+        return arg ?? "";
     }
 }
--- js/nuInputs.js
+++ js/nuInputs.js
@@ -1,7 +1,47 @@
 var nuInputs = /** @class */ (function () {
     function nuInputs() {
     }
-    nuInputs.prototype.createInputField = function (root, config) {
+    nuInputs.addInputField = function (root, config) {
+        this.__addInputFieldBox(config.className);
+        this.__addInputLabel(config.placeholder, config.className);
+        this.__addInputField(config.type, config.className, config.name, config.id, config.placeholder);
+        if (config.isRequired === true)
+            this.__addInputRequiredLabel();
+        root.appendChild(this.nuElem);
+    };
+    nuInputs.__addInputFieldBox = function (className) {
+        this.nuElem = this.__createElem("div", "inputFieldBlock", className);
+    };
+    nuInputs.__addInputLabel = function (placeholder, classLabel) {
+        var inputFieldLabel = this.__createElem("label", "inputFieldLabel", classLabel);
+        inputFieldLabel.innerText = placeholder;
+        this.nuElem.appendChild(inputFieldLabel);
+    };
+    nuInputs.__addInputField = function (type, className, name, id, placeholder) {
+        var inputField = document.createElement("input");
+        inputField.type = type !== null && type !== void 0 ? type : "text";
+        inputField.className = "inputField";
+        if (className)
+            inputField.classList.add(className);
+        inputField.id = this.__checkArg(id);
+        inputField.name = this.__checkArg(name);
+        inputField.placeholder = this.__checkArg(placeholder);
+        this.nuElem.appendChild(inputField);
+    };
+    nuInputs.__addInputRequiredLabel = function () {
+        var inputFieldRequired = this.__createElem("label", "inputFieldRequired");
+        inputFieldRequired.innerHTML = "Обязательное поле";
+        this.nuElem.appendChild(inputFieldRequired);
+    };
+    nuInputs.__createElem = function (typeElem, classNameElem, anotherClassElem) {
+        var newNuElem = document.createElement(typeElem);
+        newNuElem.className = classNameElem;
+        if (anotherClassElem)
+            newNuElem.classList.add(anotherClassElem);
+        return newNuElem;
+    };
+    nuInputs.__checkArg = function (arg) {
+        return arg !== null && arg !== void 0 ? arg : "";
     };
     return nuInputs;
 }());
